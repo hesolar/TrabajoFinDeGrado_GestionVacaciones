eaf96d5fe9000d035d72e8c9ab39930a55591d89
Hector Solar Ruiz <hectorsolar99@gmail.com>
2022-04-07 09:01:40 +0200

    roles handler

--- Application/Handlers/CommandHandlers/Delete/DeleteRolesHandler.cs
+++ Application/Handlers/CommandHandlers/Delete/DeleteRolesHandler.cs
@@ -3,7 +3,8 @@
 internal class DeleteRolesHandler : IRequestHandler<DeleteRolesCommand, bool> {
 
     private IRolesRepository _context;
-    public DeleteRolesHandler(IRolesRepository context) => this._context = context;
+    public DeleteRolesHandler(IRolesRepository context) 
+        => this._context = context;
 
     public async Task<bool> Handle(DeleteRolesCommand request, CancellationToken cancellationToken)
        => await _context.DeleteAsync(request.Id);
